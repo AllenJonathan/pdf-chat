d3ac13c5e8ffe47fef0a5ef43e92068625c8e421
AllenJonathan <allenajdjonathan@gmail.com>
2024-12-23 18:33:24 +0530

    port errors

--- app/routes.py
+++ app/routes.py
@@ -171,7 +171,7 @@ async def get(id: int):
     with Session() as session:
         print(session.query(Document).all())
     new_html = html.replace('document_id": 1', f'document_id": {id}')
-    new_html = new_html.replace("ws://localhost:port/ws", f"ws://localhost:{port}/ws/{id}")
+    new_html = new_html.replace("ws://localhost:port/ws", f"wss://localhost/ws/{id}")
     print(html)
     return HTMLResponse(new_html)
 
@@ -181,7 +181,6 @@ async def websocket_endpoint(websocket: WebSocket, id: str):
     
     await websocket.accept()
     session_context = {}
-    
 
     # Retrieve document text
     document = None
